f973f1c48791d0021bf8a54cb1f874df9d391e64
mohammed <mohammed.khedim@etu.u-pec.fr>
2025-03-19 22:09:11 +0100

    modification nom dans le code pour InsertPaiementClientRequest

---
+++ proto/xmart-frontend/src/main/java/edu/ezip/ing1/pds/requests/InsertPaiementClientRequest.java
@@ -0,0 +1,27 @@
+package edu.ezip.ing1.pds.requests;
+ 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.ezip.ing1.pds.business.dto.Paiement;
+import edu.ezip.ing1.pds.client.commons.ClientRequest;
+import edu.ezip.ing1.pds.client.commons.NetworkConfig;
+import edu.ezip.ing1.pds.commons.Request;
+ 
+import java.io.IOException;
+import java.util.Map;
+ 
+public class InsertPaiementClientRequest extends ClientRequest<Paiement, String> {
+ 
+    public InsertPaiementClientRequest(
+            NetworkConfig networkConfig, int myBirthDate, Request request, Paiement info, byte[] bytes)
+            throws IOException {
+        super(networkConfig, myBirthDate, request, info, bytes);
+    }
+ 
+    @Override
+    public String readResult(String body) throws IOException {
+        final ObjectMapper mapper = new ObjectMapper();
+        final Map<String, Integer> paiementIdMap = mapper.readValue(body, Map.class);
+        final String result  = paiementIdMap.get("idPaiement").toString();
+        return result;
+    }
+}
