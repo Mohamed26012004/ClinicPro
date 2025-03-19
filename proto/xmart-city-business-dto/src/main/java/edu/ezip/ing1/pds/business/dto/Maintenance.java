df9772453d4cb231f675a8a5e1d34b1488a44f5d
SMax.2465 <maximesoupgui@gmail.com>
2025-03-19 20:58:04 +0100

    Gestion des conflits.
    Proplème avec la classe Maintenance.java que mon collègue va corriger.

--- proto/xmart-city-business-dto/src/main/java/edu/ezip/ing1/pds/business/dto/Maintenance.java
+++ proto/xmart-city-business-dto/src/main/java/edu/ezip/ing1/pds/business/dto/Maintenance.java
@@ -16,68 +16,69 @@ public class Maintenance {
     private  int coutMaintenance;
     private  int idMaintenance;
     private Date dateMaintenace;
+    private String nomEquipement;
 
 
     public Maintenance() {
     }
-    public final Maintenance build(final ResultSet resultSet)
-            throws SQLException, NoSuchFieldException, IllegalAccessException {
-        setFieldsFromResulset(resultSet, "idMaintenance", "typeMaintenance","coutMaintenance", "dateMaintenance");
-        return this;
+//    public final Maintenance build(final ResultSet resultSet)
+//            throws SQLException, NoSuchFieldException, IllegalAccessException {
+//        setFieldsFromResulset(resultSet, "idMaintenance", "typeMaintenance","coutMaintenance", "dateMaintenance");
+//        return this;
+//
+//    }
+//    public final PreparedStatement build(PreparedStatement preparedStatement)
+//            throws SQLException, NoSuchFieldException, IllegalAccessException {
+//        return buildPreparedStatement(preparedStatement, String.valueOf(idMaintenance),String.valueOf(coutMaintenance), typeMaintenance, dateMaintenace.toString());
+//    }
 
-    }
-    public final PreparedStatement build(PreparedStatement preparedStatement)
-            throws SQLException, NoSuchFieldException, IllegalAccessException {
-        return buildPreparedStatement(preparedStatement, String.valueOf(idMaintenance),String.valueOf(coutMaintenance), typeMaintenance, dateMaintenace.toString());
-    }
-
-    public Maintenance(int coutMaintenance, String typeMaintenance, int idMaintenance, Date dateMaintenance) {
-        this.nomEquipement = nomEquipement;
-        this.coutEquipement = coutEquipement;
-    }
-
-    public int   getCoutEquipement() {
-        return coutEquipement;
-    }
-
-    public int  getIdEquipement() {
-        return idEquipement;
-    }
-
-    public String getNomEquipement() {
-        return nomEquipement;
-    }
-    public Date getDateEquipement(){
-        return dateEquipement;
-    }
-
-
-    @JsonProperty("equipement_coutEquipement")
-    public void setCoutEquipement(int coutEquipement) {
-        this.coutEquipement = coutEquipement;
-    }
-
-
-    @JsonProperty("equipement_idEquipement")
-    public void setIdEquipement(int idEquipement) {
-        this.idEquipement = idEquipement;
-    }
-
-    @JsonProperty("equipement_nomEquipement")
-    public void setNomEquipement(String nomEquipement) {
-        this.nomEquipement = nomEquipement;
-    }
-    @JsonProperty("equipement_dateEquipement")
-    public void setDateEquipement(Date dateEquipement) {
-        this.dateEquipement = dateEquipement;
-    }
-    private void setFieldsFromResulset(final ResultSet resultSet, final String ... fieldNames )
-            throws NoSuchFieldException, SQLException, IllegalAccessException {
-        for(final String fieldName : fieldNames ) {
-            final Field field = this.getClass().getDeclaredField(fieldName);
-            field.set(this, resultSet.getObject(fieldName));
-        }
-    }
+//    public Maintenance(int coutMaintenance, String typeMaintenance, int idMaintenance, Date dateMaintenance) {
+//        this.nomEquipement = nomEquipement;
+//        this.coutEquipement = coutEquipement;
+//    }
+//
+//    public int   getCoutEquipement() {
+//        return coutEquipement;
+//    }
+//
+//    public int  getIdEquipement() {
+//        return idEquipement;
+//    }
+//
+//    public String getNomEquipement() {
+//        return nomEquipement;
+//    }
+//    public Date getDateEquipement(){
+//        return dateEquipement;
+//    }
+//
+//
+//    @JsonProperty("equipement_coutEquipement")
+//    public void setCoutEquipement(int coutEquipement) {
+//        this.coutEquipement = coutEquipement;
+//    }
+//
+//
+//    @JsonProperty("equipement_idEquipement")
+//    public void setIdEquipement(int idEquipement) {
+//        this.idEquipement = idEquipement;
+//    }
+//
+//    @JsonProperty("equipement_nomEquipement")
+//    public void setNomEquipement(String nomEquipement) {
+//        this.nomEquipement = nomEquipement;
+//    }
+//    @JsonProperty("equipement_dateEquipement")
+//    public void setDateEquipement(Date dateEquipement) {
+//        this.dateEquipement = dateEquipement;
+//    }
+//    private void setFieldsFromResulset(final ResultSet resultSet, final String ... fieldNames )
+//            throws NoSuchFieldException, SQLException, IllegalAccessException {
+//        for(final String fieldName : fieldNames ) {
+//            final Field field = this.getClass().getDeclaredField(fieldName);
+//            field.set(this, resultSet.getObject(fieldName));
+//        }
+//    }
     private final PreparedStatement buildPreparedStatement(PreparedStatement preparedStatement, final String ... fieldNames )
             throws NoSuchFieldException, SQLException, IllegalAccessException {
         int ix = 0;
@@ -87,15 +88,15 @@ public class Maintenance {
         return preparedStatement;
     }
 
-    @Override
-    public String toString() {
-        return "equipement{" +
-                "coutEquipement='" + coutEquipement + '\'' +
-                ", idEquipement='" + idEquipement + '\'' +
-                ", nomEquipement='" + nomEquipement + '\'' +
-                "dateEquipement='" + dateEquipement + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "equipement{" +
+//                "coutEquipement='" + coutEquipement + '\'' +
+//                ", idEquipement='" + idEquipement + '\'' +
+//                ", nomEquipement='" + nomEquipement + '\'' +
+//                "dateEquipement='" + dateEquipement + '\'' +
+//                '}';
+//    }
 
 }
 
