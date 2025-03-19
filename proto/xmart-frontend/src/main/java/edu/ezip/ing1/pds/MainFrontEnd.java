7dfad4af8381749c45d7785436fede6e00fedfb7
Mohamed26012004 <raslendkhili76@gmail.com>
2025-03-19 18:01:06 +0100

    commit.

--- proto/xmart-frontend/src/main/java/edu/ezip/ing1/pds/MainFrontEnd.java
+++ proto/xmart-frontend/src/main/java/edu/ezip/ing1/pds/MainFrontEnd.java
@@ -1,36 +1,42 @@
 package edu.ezip.ing1.pds;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
-import edu.ezip.ing1.pds.business.dto.Facture;
-
-import java.util.Date;
-
-
-import edu.ezip.ing1.pds.business.dto.Factures;
+import edu.ezip.ing1.pds.business.dto.Equipement;
 import edu.ezip.ing1.pds.client.commons.ConfigLoader;
 import edu.ezip.ing1.pds.client.commons.NetworkConfig;
-import edu.ezip.ing1.pds.graphics.Fenetre;
-import edu.ezip.ing1.pds.services.ExamenService;
-import edu.ezip.ing1.pds.services.FactureService;
+import edu.ezip.ing1.pds.services.EquipementService;
 
 public class MainFrontEnd {
 
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        /*Fenetre fen = new Fenetre();
+        fen.setVisible(true);*/
 
-        Fenetre fen = new Fenetre();
-        fen.setVisible(true);
-//
-//        Date dat = new Date();
-//        Facture fac = new Facture(true, dat);
-//        final String networkConfigFile = "network.yaml";
-//        final NetworkConfig networkConfig = ConfigLoader.loadConfig(NetworkConfig.class, networkConfigFile);
-//        final FactureService factureService = new FactureService(networkConfig);
-//
-//        factureService.insertFacture(fac);
-//        Factures factures = factureService.selectFactures();
-//        System.out.println(factures);
+        final String networkConfigFile = "network.yaml";
+        final NetworkConfig networkConfig = ConfigLoader.loadConfig(NetworkConfig.class, networkConfigFile);
+        final EquipementService service = new EquipementService(networkConfig);
+        LocalDate date = LocalDate.of(2025, 3, 12);
+        Equipement equipement = new Equipement(5, "table", date, 4);
+        service.insertEquipement(equipement);
+
+
+
+        /*final String networkConfigFile = "network.yaml";
+        final NetworkConfig networkConfig = ConfigLoader.loadConfig(NetworkConfig.class, networkConfigFile);
+        final EquipementService equipementService = new EquipementService(networkConfig);
+        System.out.println(equipementService.selectEquipements());
+        System.out.println(service.selctequipements());*/
+
 
     }
 }
+
+
+
+
+
+ 
+
