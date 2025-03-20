bf765786805e8499069722fe6ac47605d53732da
Mohamed26012004 <raslendkhili76@gmail.com>
2025-03-20 11:22:58 +0100

    TESTE

--- proto/xmart-frontend/src/main/java/edu/ezip/ing1/pds/MainFrontEnd.java
+++ proto/xmart-frontend/src/main/java/edu/ezip/ing1/pds/MainFrontEnd.java
@@ -6,29 +6,15 @@ import java.time.LocalDate;
 import edu.ezip.ing1.pds.business.dto.Equipement;
 import edu.ezip.ing1.pds.client.commons.ConfigLoader;
 import edu.ezip.ing1.pds.client.commons.NetworkConfig;
+import edu.ezip.ing1.pds.graphics.Fenetre;
 import edu.ezip.ing1.pds.services.EquipementService;
 
 public class MainFrontEnd {
 
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        /*Fenetre fen = new Fenetre();
-        fen.setVisible(true);*/
-
-        final String networkConfigFile = "network.yaml";
-        final NetworkConfig networkConfig = ConfigLoader.loadConfig(NetworkConfig.class, networkConfigFile);
-        final EquipementService service = new EquipementService(networkConfig);
-        LocalDate date = LocalDate.of(2025, 3, 12);
-        Equipement equipement = new Equipement(5, "table", date, 4);
-        service.insertEquipement(equipement);
-
-
-
-        /*final String networkConfigFile = "network.yaml";
-        final NetworkConfig networkConfig = ConfigLoader.loadConfig(NetworkConfig.class, networkConfigFile);
-        final EquipementService equipementService = new EquipementService(networkConfig);
-        System.out.println(equipementService.selectEquipements());
-        System.out.println(service.selctequipements());*/
+        Fenetre fen = new Fenetre();
+        fen.setVisible(true);
 
 
     }
