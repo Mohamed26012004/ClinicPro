fe8fa6991352f0d813433571119eff1fb83af87a
mohammed <mohammed.khedim@etu.u-pec.fr>
2025-05-12 18:59:55 +0200

    Modification des requetes et méthodes Factures dans XmartCity

--- proto/xmart-city-backend/src/main/java/edu/ezip/ing1/pds/business/server/XMartCityService.java
+++ proto/xmart-city-backend/src/main/java/edu/ezip/ing1/pds/business/server/XMartCityService.java
@@ -37,11 +37,11 @@ public class XMartCityService {
         SELECT_ONE_EXAMEN("SELECT t.id, t.nom, t.cout, t.duree FROM examen t WHERE nom = ? AND cout = ? AND duree = ?"),
         ID_EXAMEN("SELECT id FROM examen WHERE nom = ? AND cout = ? AND duree = ?"),
 
-        SELECT_ALL_FACTURES("SELECT t.idFacture, t.dateFacture, t.regle FROM facture t"),
-        INSERT_FACTURE("INSERT into facture (dateFacture, regle) values (?, ?)"),
-        UPDATE_FACTURE("UPDATE facture SET dateFacture = ?, regle = ? WHERE idFacture = ?"),
+        SELECT_ALL_FACTURES("SELECT t.idFacture, t.dateFacture, t.montantFacture, t.regle, t.idPatient, t.idExamen FROM facture t"),
+        INSERT_FACTURE("INSERT INTO facture (montantFacture, regle, dateFacture, idPatient, idExamen) VALUES (?, ?, ?, ?, ?)"),
+        UPDATE_FACTURE("UPDATE facture SET montantFacture = ?, regle = ?, dateFacture = ?, idPatient = ?, idExamen = ? WHERE idFacture = ?"),
         DELETE_FACTURE("DELETE FROM facture WHERE idFacture = ?"),
-        ID_FACTURE("SELECT idFacture FROM facture WHERE dateFacture = ? AND regle = ?"),
+        ID_FACTURE("SELECT idFacture FROM facture WHERE montantFacture = ? AND regle = ? AND dateFacture = ? AND idPatient = ? AND idExamen = ?"),
 
         FACTURES_PAYEES("SELECT t.idFacture, t.dateFacture FROM facture t WHERE t.regle=true"),
         FACTURES_QUOTIDIENNES("SELECT idFacture, regle FROM facture WHERE dateFacture = ?"),
@@ -514,55 +514,70 @@ public class XMartCityService {
     }
 
     // Méthodes des requêtes sur les factures
-
+ 
     private Response SelectAllFactures(final Request request, final Connection connection) throws SQLException, JsonProcessingException {
         final ObjectMapper objectMapper = new ObjectMapper();
         final Statement stmt = connection.createStatement();
         final ResultSet res = stmt.executeQuery(Queries.SELECT_ALL_FACTURES.query);
         Factures factures = new Factures();
+       
         while (res.next()) {
             Facture facture = new Facture();
-            facture.setIdFacture(res.getInt(1));
-            facture.setDateFacture(res.getString(2));
-            facture.setRegle(res.getBoolean(3));
+            facture.setIdFacture(res.getInt(1));                        
+            facture.setDateFacture(res.getDate(2).toLocalDate());      
+            facture.setMontantFacture(res.getDouble(3));                
+            facture.setRegle(res.getBoolean(4));                        
+            facture.setIdPatient(res.getInt(5));                        
+            facture.setIdExamen(res.getInt(6));                        
             factures.add(facture);
         }
+   
         return new Response(request.getRequestId(), objectMapper.writeValueAsString(factures));
     }
-
+   
+ 
     private Response InsertFacture(final Request request, final Connection connection) throws SQLException, IOException {
         final ObjectMapper objectMapper = new ObjectMapper();
         final Facture facture = objectMapper.readValue(request.getRequestBody(), Facture.class);
-
+   
         final PreparedStatement stmt = connection.prepareStatement(Queries.INSERT_FACTURE.query);
-        stmt.setString(1, facture.getDateFacture());
-        stmt.setBoolean(2, facture.getRegle());
+        stmt.setDate(1, Date.valueOf(facture.getDateFacture()));  
+        stmt.setDouble(2, facture.getMontantFacture());            
+        stmt.setBoolean(3, facture.getRegle());                    
+        stmt.setInt(4, facture.getIdPatient());                    
+        stmt.setInt(5, facture.getIdExamen());                    
+   
         stmt.executeUpdate();
-
+   
         return new Response(request.getRequestId(), objectMapper.writeValueAsString(facture));
     }
-
+   
+ 
     private Response UpdateFacture(final Request request, final Connection connection) throws SQLException, IOException {
         final ObjectMapper objectMapper = new ObjectMapper();
         final Facture facture = objectMapper.readValue(request.getRequestBody(), Facture.class);
-
+   
         final PreparedStatement stmt = connection.prepareStatement(Queries.UPDATE_FACTURE.query);
-        stmt.setString(1, facture.getDateFacture());
-        stmt.setBoolean(2, facture.getRegle());
-        stmt.setInt(3, facture.getIdFacture());
+        stmt.setDate(1, Date.valueOf(facture.getDateFacture()));  
+        stmt.setDouble(2, facture.getMontantFacture());            
+        stmt.setBoolean(3, facture.getRegle());                    
+        stmt.setInt(4, facture.getIdPatient());                    
+        stmt.setInt(5, facture.getIdExamen());                    
+        stmt.setInt(6, facture.getIdFacture());                    
+   
         stmt.executeUpdate();
-
+   
         return new Response(request.getRequestId(), objectMapper.writeValueAsString(facture));
     }
-
+ 
     private Response DeleteFacture(final Request request, final Connection connection) throws SQLException, IOException {
         final ObjectMapper objectMapper = new ObjectMapper();
         final Facture facture = objectMapper.readValue(request.getRequestBody(), Facture.class);
-
+ 
         final PreparedStatement stmt = connection.prepareStatement(Queries.DELETE_FACTURE.query);
         stmt.setInt(1, facture.getIdFacture());
         stmt.executeUpdate();
-
+ 
         return new Response(request.getRequestId(), objectMapper.writeValueAsString(facture));
     }
 
@@ -574,7 +589,7 @@ public class XMartCityService {
         while (res.next()) {
             Facture facture = new Facture();
             facture.setIdFacture(res.getInt(1));
-            facture.setDateFacture(res.getString(2));
+            //facture.setDateFacture(res.getString(2));
             factures.add(facture);
         }
         return new Response(request.getRequestId(), objectMapper.writeValueAsString(factures));
