0846752694857104681858d3718c43bda266e376
mohammed <mohammed.khedim@etu.u-pec.fr>
2025-05-12 18:51:30 +0200

    Modification de Paiement.java

--- proto/xmart-city-business-dto/src/main/java/edu/ezip/ing1/pds/business/dto/Paiement.java
+++ proto/xmart-city-business-dto/src/main/java/edu/ezip/ing1/pds/business/dto/Paiement.java
@@ -1,72 +1,100 @@
 package edu.ezip.ing1.pds.business.dto;
-
+ 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
-
+ 
 import java.lang.reflect.Field;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+ 
 @JsonRootName(value = "paiement")
 public class Paiement {
     private int idPaiement;
     private  double montant;
-    private  String datePaiement;
+ 
+    @JsonDeserialize(using = DeserialisationDate.class) // doit etre au dessus des dates
+    private LocalDate datePaiement;
+ 
+    private final DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+ 
     private  String moyenDePaiement;
-
+    private int idFacture;
+    private int idPatient;
+ 
     public Paiement() {
     }
     public final Paiement build(final ResultSet resultSet)
             throws SQLException, NoSuchFieldException, IllegalAccessException {
-        setFieldsFromResulset(resultSet, "idPaiement", "montant","datePaiement", "moyenDePaiement");
+        setFieldsFromResulset(resultSet, "idPaiement", "montant", "datePaiement", "moyenDePaiement", "idFacture", "idPatient");
         return this;
     }
     public final PreparedStatement build(PreparedStatement preparedStatement)
             throws SQLException, NoSuchFieldException, IllegalAccessException {
-        return buildPreparedStatement(preparedStatement, String.valueOf(idPaiement), String.valueOf(montant), datePaiement, moyenDePaiement);
+        return buildPreparedStatement(preparedStatement, String.valueOf(idPaiement), String.valueOf(montant), datePaiement.format(formatDate), moyenDePaiement, String.valueOf(idFacture), String.valueOf(idPatient));
     }
-
-    public Paiement(double montant, String datePaiement, String moyenDePaiement) {
+ 
+    public Paiement(double montant, LocalDate datePaiement, String moyenDePaiement, int idFacture, int idPatient) {
         this.montant = montant;
         this.datePaiement = datePaiement;
         this.moyenDePaiement = moyenDePaiement;
+        this.idFacture = idFacture;
+        this.idPatient = idPatient;
     }
-
+ 
     public int getidPaiement() {
         return idPaiement;
     }
     public double getmontant() {
         return montant;
     }
-    public String getdatePaiement() {
+    public LocalDate getdatePaiement() {
         return datePaiement;
     }
     public String getmoyenDePaiement() {
         return moyenDePaiement;
     }
-    
+    public int getidFacture() {
+        return idFacture;
+    }
+    public int getidPatient() {
+        return idPatient;
+    }
+   
     @JsonProperty("idPaiement")
     public void setidPaiement(int idPaiement) {
         this.idPaiement = idPaiement;
     }
-
+ 
     @JsonProperty("montant")
     public void setmontant(double montant) {
         this.montant = montant;
     }
-
+ 
     @JsonProperty("datePaiement")
-    public void setdatePaiement(String datePaiement) {
+    public void setdatePaiement(LocalDate datePaiement) {
         this.datePaiement = datePaiement;
     }
-
+ 
     @JsonProperty("moyenDePaiement")
     public void setmoyenDePaiement(String moyenDePaiement) {
         this.moyenDePaiement = moyenDePaiement;
     }
-
-
+ 
+    @JsonProperty("idFacture")
+    public void setidFacture(int idFacture) {
+        this.idFacture = idFacture;
+    }
+ 
+    @JsonProperty("idPatient")
+    public void setidPatient(int idPatient) {
+        this.idPatient = idPatient;
+    }
+ 
+ 
     private void setFieldsFromResulset(final ResultSet resultSet, final String ... fieldNames )
             throws NoSuchFieldException, SQLException, IllegalAccessException {
         for(final String fieldName : fieldNames ) {
@@ -82,7 +110,7 @@ public class Paiement {
         }
         return preparedStatement;
     }
-
+ 
     @Override
     public String toString() {
         return "Paiement{" +
@@ -90,6 +118,8 @@ public class Paiement {
                 ", montant='" + montant + '\'' +
                 ", datePaiement='" + datePaiement + '\'' +
                 ", moyenDePaiement='" + moyenDePaiement + '\'' +
+                ", idFacture=" + idFacture +
+                ", idPatient=" + idPatient +
                 '}';
     }
 }
