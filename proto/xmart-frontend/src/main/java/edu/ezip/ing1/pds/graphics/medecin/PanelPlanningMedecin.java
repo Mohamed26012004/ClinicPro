edd13238a4391bab4d0a723de29c3c72fb9afa36
SMax.2465 <maximesoupgui@gmail.com>
2025-05-14 21:05:18 +0200

    Ajout de la fonctionnalité empêchant la réservation d'un créneau d'un jour déjà passé.

--- proto/xmart-frontend/src/main/java/edu/ezip/ing1/pds/graphics/medecin/PanelPlanningMedecin.java
+++ proto/xmart-frontend/src/main/java/edu/ezip/ing1/pds/graphics/medecin/PanelPlanningMedecin.java
@@ -363,10 +363,16 @@ public class PanelPlanningMedecin extends JPanel {
             public void actionPerformed(ActionEvent e) {
                 int i = tableDisponibilite.getSelectedRow();
                 if (i > 0){
-                    planificationDuMedecin.setHeureDebut(LocalTime.parse(modelDisponibilite.getValueAt(i, 0).toString(), formatter));
-                    planificationDuMedecin.setHeureFin(LocalTime.parse(modelDisponibilite.getValueAt(i, 1).toString(), formatter));
-                    FrameInsertUpdatePlanification f = new FrameInsertUpdatePlanification(planificationDuMedecin);
+                    if(planificationDuMedecin.getDatePlanification().isBefore(LocalDate.now())){
+                        JOptionPane.showMessageDialog(null, "La date sélectionnée est déjà passée. Veuillez choisir une date future.", "Erreur", JOptionPane.ERROR_MESSAGE);
+                    }else{
+                        planificationDuMedecin.setHeureDebut(LocalTime.parse(modelDisponibilite.getValueAt(i, 0).toString(), formatter));
+                        planificationDuMedecin.setHeureFin(LocalTime.parse(modelDisponibilite.getValueAt(i, 1).toString(), formatter));
+                        FrameInsertUpdatePlanification f = new FrameInsertUpdatePlanification(planificationDuMedecin);
+                    }
                 }
+
+
             }
         });
 
